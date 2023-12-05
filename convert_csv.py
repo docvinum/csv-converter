7cc1272d5e9906f65aa696370b68d005e6bba84d
Alexandre Basta <42250730+docvinum@users.noreply.github.com>
2023-12-05 11:36:21 +0100

    Update convert_csv.py
    ### Modifications Apportées au Script de Conversion CSV
    
    **1. Gestion des Erreurs d'Encodage et de Détection du Délimiteur :**
       - Ajouté la gestion des erreurs pour résoudre les problèmes liés à l'encodage incohérent des fichiers CSV.
       - Mis en place un mécanisme pour gérer les erreurs lors de la tentative de détection automatique du délimiteur dans les fichiers CSV.
    
    **2. Amélioration de l'Affichage des Informations des Fichiers :**
       - Modifié l'affichage des fichiers CSV trouvés dans le répertoire pour les lister un par ligne, améliorant ainsi la lisibilité.
       - Implémenté l'affichage des informations sur les fichiers CSV (nom, encodage, délimiteur) sous forme de tableau pour une meilleure clarté.
    
    **3. Interaction Utilisateur pour la Saisie du Délimiteur :**
       - Supprimé la détection automatique du délimiteur à cause de son inefficacité dans certains cas.
       - Ajouté une fonctionnalité pour demander à l'utilisateur d'entrer manuellement le caractère délimiteur pour chaque fichier CSV avant la conversion.
       - Cette modification permet de traiter des fichiers avec des formats de délimitation moins standards ou incohérents.
    
    **4. Optimisation de la Conversion des Fichiers :**
       - Adapté le script pour convertir les fichiers CSV sélectionnés en UTF-8, avec le délimiteur fourni par l'utilisateur, améliorant la compatibilité et l'uniformité des fichiers CSV traités.
    
    Ces améliorations rendent le script plus robuste, flexible et adapté à une variété de formats et d'encodages de fichiers CSV.

--- convert_csv.py
+++ convert_csv.py
@@ -7,15 +7,14 @@ def detect_encoding(file_path):
         result = chardet.detect(f.read())
     return result['encoding']
 
-def detect_delimiter(file_path, encoding):
-    with open(file_path, 'r', encoding=encoding) as f:
-        sniffer = csv.Sniffer()
-        return sniffer.sniff(f.readline()).delimiter
-
 def convert_csv(file_path, encoding, delimiter):
-    with open(file_path, 'r', encoding=encoding) as f:
-        reader = csv.reader(f, delimiter=delimiter)
-        rows = list(reader)
+    try:
+        with open(file_path, 'r', encoding=encoding) as f:
+            reader = csv.reader(f, delimiter=delimiter)
+            rows = list(reader)
+    except UnicodeDecodeError:
+        print(f"Erreur d'encodage lors de la lecture de {file_path}. Conversion ignorée.")
+        return
 
     new_file_path = os.path.splitext(file_path)[0] + "_converted.csv"
     with open(new_file_path, 'w', encoding='utf-8', newline='') as f:
@@ -25,12 +24,10 @@ def convert_csv(file_path, encoding, delimiter):
 
 def main():
     csv_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.csv')]
-    print("Found CSV files:", csv_files)
-
+    
+    print("Found CSV files:")
     for file in csv_files:
-        encoding = detect_encoding(file)
-        delimiter = detect_delimiter(file, encoding)
-        print(f"File: {file}, Encoding: {encoding}, Delimiter: '{delimiter}'")
+        print(f"- {file}")
 
     choice = input("Enter file names to convert (comma-separated) or 'all' to convert all files: ").strip()
     if choice.lower() == 'all':
@@ -40,9 +37,11 @@ def main():
 
     for file in files_to_convert:
         if file in csv_files:
+            encoding = detect_encoding(file)
+            delimiter = input(f"Enter the delimiter for {file}: ")
             print(f"Converting {file}...")
-            convert_csv(file, detect_encoding(file), detect_delimiter(file, detect_encoding(file)))
-            print(f"Converted {file} to UTF-8 with comma as delimiter.")
+            convert_csv(file, encoding, delimiter)
+            print(f"Converted {file} to UTF-8 with '{delimiter}' as delimiter.")
 
 if __name__ == "__main__":
     main()
